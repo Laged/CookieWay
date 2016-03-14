6ca826fc6a0062a4952731e6ca4a54733b27c954
Your Name <you@example.com>
2016-03-14 14:05:48 +0200

    match header with source

--- surveyfiller.h
+++ surveyfiller.h
@@ -2,12 +2,24 @@
 #define SURVEYFILLER_H
 #include <QString>
 
+#include <QWebView>
+#include <QUrl>
+#include <QObject>
+#include <QWebElement>
 
-class SurveyFiller
+class SurveyFiller: public QObject
 {
+    Q_OBJECT
+
 public:
     SurveyFiller();
     QString fillSurvey(QStringList surveyData);
+public slots:
+    void printkalja(bool a);
+private:
+    QWebView *webView;
+
+
 };
 
 #endif // SURVEYFILLER_H
